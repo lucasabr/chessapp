55eed02e8c07c29aed9cc7e925737e21c9ef28c9
Lucas Aguiar <lucasabr2001@gmail.com>
2020-01-08 16:07:09 -0500

    template

--- chess/views.py
+++ chess/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+messages = [
+    {
+        'author' : 'Lucasabr',
+        'recipient' : 'Jmilicev',
+        'message' : 'Whats up',
+        'timestamp' : 'Jan 8th, 2019'
+    },
+    {
+        'author' : 'Jmilicev',
+        'recipient' : 'Lucasabr',
+        'message' : 'Nmu',
+        'timestamp' : 'Jan 8th, 2019'
+    }
+]
 def home(request):
-    return HttpResponse('<h1>Welcome to Chess</h>')
+    context = {
+        'messages' : messages
+    }
+    return render(request, 'chess/home.html', context)
