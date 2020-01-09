2b167535d5dfd907f6c37af874c6956a889308e5
Lucas Aguiar <lucasabr2001@gmail.com>
2020-01-09 18:37:58 -0500

    .

--- ttt/models.py
+++ ttt/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Game(models.Model):
+    date_played = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.SET("DeletedUser"))
+    
+    def __str__(self):
+        return self.author
